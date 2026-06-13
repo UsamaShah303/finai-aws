28619cef3ca05ff6e06da8ff1905fc1f4f0bfdcf
UsamaShah303 <usama.sh303@gmail.com>
2026-06-14 00:57:56 +0500

    Switch to us-east-2 to avoid VPC limits

--- infra/network.tf
+++ infra/network.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name = "finai-public-subnet"
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "finai-private-subnet-1"
@@ -35,7 +35,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-2b"
 
   tags = {
     Name = "finai-private-subnet-2"
